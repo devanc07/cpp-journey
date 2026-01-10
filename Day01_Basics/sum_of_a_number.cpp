b35df29fd7eda46238b3c853a434d89662ed72de
Deva Nanda C <nc07deva@gmail.com>
2026-01-10 17:18:18 +0530

    Add sum_of_a_number.cpp to calculate sum of two numbers

---
+++ Day01_Basics/sum_of_a_number.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, sum;
+    cout<<"Enter a number : ";
+    cin >>a;
+    cout<< "Enter another number : ";
+    cin >>b;
+    sum = a+b;
+    cout<<"The sum is : " << sum;
+    return 0;
+}
